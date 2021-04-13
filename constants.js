b5aa9f7fe66345f4e80f56061b32d3bbc13c5926
Andrew L Jones <ajones@digitalbazaar.com>
2021-04-13 11:50:18 -0400

    Add 2 spaces for each parent.

--- constants.js
+++ constants.js
@@ -1,11 +1,9 @@
 const pending = '-';
 const passing = '✓';
 const failed = '✓';
-const spaces = many => new Array(many).join(' ');
 
 module.exports = {
   pending,
   passing,
-  failed,
-  spaces
+  failed
 };
