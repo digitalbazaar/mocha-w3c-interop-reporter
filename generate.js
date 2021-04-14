a7aa50b17206db527cfb110522179051c6c9ad97
Andrew L Jones <ajones@digitalbazaar.com>
2021-04-14 16:42:44 -0400

    Create partial for each table & matrix.

--- generate.js
+++ generate.js
@@ -13,16 +13,20 @@ const {writeJSON, getPartial} = require('./files.js');
 //masterName is the name of the main report
 //used to get the title of all tests for the spec
 async function makeReport({fileName, report}) {
+  const {templates, respecConfig} = testConfig;
   const context = {
     matrices: [],
-    tables: []
+    tables: [
+      {title: 'one', columnNames: ['col one'], tests: [{state: 'passed', optional: false}]},
+      {title: 'two', columnNames: ['one'], tests: []}
+    ],
+    respecConfig: JSON.stringify(respecConfig)
   };
   for(const key in report) {
     const suite = report[key];
     console.log(Object.keys(suite));
     console.log(Object.keys(suite[0]));
   }
-  const {templates} = testConfig;
   // go ahead and load these partial templates
   // from disk into handlebars
   await getPartial({filePath: templates.head, name: 'head.hbs'});
@@ -31,8 +35,8 @@ async function makeReport({fileName, report}) {
   await getPartial({filePath: templates.table, name: 'table.hbs'});
   await getPartial({filePath: templates.matrix, name: 'matrix.hbs'});
   const template = makeTemplate();
-  const result = template({context});
-//console.log(result);
+  const result = template(context);
+console.log(result);
   return context;
 }
 
