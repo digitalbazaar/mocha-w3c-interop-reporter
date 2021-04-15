999a1cba1114ff8694967841fe2bf84394e520b8
Andrew L Jones <ajones@digitalbazaar.com>
2021-04-15 09:43:45 -0400

    Add circular reg replacer and raw mocha test results to test project.

--- constants.js
+++ constants.js
@@ -2,26 +2,8 @@ const pending = '-';
 const passing = '✓';
 const failed = '❌';
 
-//no circular JSON props for tests
-const testProps = [
-  'optional', 'type',
-  'title', 'body',
-  'async', 'sync',
-  '_timeout', '_slow',
-  '_retries', 'timedOut',
-  '_currentRetry', 'pending',
-  'file', 'parent',
-  'ctx', '_events',
-  '_eventsCount', 'callback',
-  'timer', 'duration',
-  'state', 'speed',
-  'fullTitle', 'errors',
-  'matrix', 'table'
-];
-
 module.exports = {
   pending,
   passing,
   failed,
-  testProps
 };
