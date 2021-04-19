961e4cd496876123a45c781b1305c7907b956279
Andrew L Jones <ajones@digitalbazaar.com>
2021-04-19 10:44:47 -0400

    Add chai and assertion shouldBeReport.

--- test/mocha/10-render.js
+++ test/mocha/10-render.js
@@ -7,16 +7,20 @@ require = require('esm')(module);
 
 const {singleMatrix, multipleMatrices} = require('../mock-data');
 const {makeReport} = require('../../generate');
+const {shouldBeReport} = require('../assertions');
+
 // FIXME remove this
 //const {asyncWriteFile} = require('../../files');
 
 describe('generate', async function() {
   it('should render a matrix', async function() {
     const report = await makeReport({suite: singleMatrix});
+    shouldBeReport(report);
     console.log('single report', report);
   });
   it('should render multiple matrices', async function() {
     const report = await makeReport({suite: multipleMatrices});
+    shouldBeReport(report);
     console.log('multiple report', report);
   });
 });
