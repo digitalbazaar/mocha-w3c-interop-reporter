d04a53e02df12f97d47803d2a3bfaa20ec01ae9b
Andrew L Jones <ajones@digitalbazaar.com>
2021-04-19 10:56:36 -0400

    Regen expected reports & assert expected reports.

--- test/assertions.js
+++ test/assertions.js
@@ -3,5 +3,8 @@ const should = chai.should();
 export const shouldBeReport = (actualReport, expectedReport) => {
   should.exist(actualReport, 'Expected report to exist');
   actualReport.should.be.a('string', 'Expected report to be a string');
-  //actualReport.should.deep.equal(expectedReport);
+  should.exist(expectedReport, 'Expected expectedReport to exist');
+  expectedReport.should.be.a(
+    'string', 'Expected expectedReport to be a string');
+  actualReport.should.deep.equal(expectedReport);
 };
