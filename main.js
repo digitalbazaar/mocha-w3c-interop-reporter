d9a4beb45975e282c0015d1da026ec4afe75e64d
Andrew L Jones <ajones@digitalbazaar.com>
2021-04-19 19:56:36 -0400

    Log test error on failure.

--- main.js
+++ main.js
@@ -60,6 +60,7 @@ function InteropReporter(runner, options = {}) {
     console.log(spaces(parents(test) * 2), Chalk.green(passing), test.title);
   }).on(EVENT_TEST_FAIL, test => {
     console.log(spaces(parents(test) * 2), Chalk.red(failed), test.title);
+    console.error(test.err);
   }).on(EVENT_RUN_END, async function() {
     try {
       const reportHTML = await makeReport({suite: rootSuite});
