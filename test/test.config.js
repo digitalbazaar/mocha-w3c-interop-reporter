900248f430d43853b3df24c14f11a6c4abefd6ba
Andrew L Jones <ajones@digitalbazaar.com>
2021-04-19 10:34:27 -0400

    Remove report.html & add jsdoc to utils.

--- test/test.config.js
+++
@@ -1,1 +0,0 @@
-const interopReporter = require('..');
--- utils.js
+++ utils.js
@@ -3,7 +3,24 @@
  */
 'use strict';
 
+/**
+ * Creates white spaces.
+ *
+ * @param {number} many - How many white spaces?
+ *
+ * @returns {string} A string contains white spaces.
+*/
 const spaces = many => new Array(many).join(' ');
+
+/**
+ * Counts how many parents a test/ suite has.
+ *
+ * @param {object} test - A mocha test or suite.
+ * @param {number} [count=0] - The number of parents counted.
+ *
+ * @returns {number|Function} Returns a number when it's done
+ *   else it returns itself.
+*/
 const parents = (test, count = 0) => {
   if(!test.parent) {
     return count;
