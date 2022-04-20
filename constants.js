bf557a2aeaad0b780c3742b6da39a4b62b88e162
Andrew Jones <ajones@digitalbazaar.com>
2022-04-20 16:35:47 -0500

    Add expanded status marks to Handlebars helper.

--- constants.js
+++ constants.js
@@ -1,11 +1,24 @@
 const pending = '🛑';
 const passing = '✓';
-const failed = '❌';
+const failing = '❌';
 const accessDenied = '⊘';
 const timeout = '⏱';
 const notImplemented = '—';
 
+const statusMarks = {
+  pending,
+  passing,
+  failing,
+  accessDenied,
+  timeout,
+  notImplemented,
+  // this mocha's test states aren't in present tense
+  'passed': passing,
+  'failed': failing,
+};
+
 module.exports = {
+  statusMarks,
   pending,
   passing,
   failed,
