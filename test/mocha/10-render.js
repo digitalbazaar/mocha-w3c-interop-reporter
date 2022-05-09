664a391ecd592a91493ebcd197b3c857b02d58b9
Andrew Jones <ajones@digitalbazaar.com>
2022-05-09 16:45:23 -0400

    Replace __dirname with es6 export friendly version.

--- test/mocha/10-render.js
+++ test/mocha/10-render.js
@@ -6,19 +6,24 @@
 import {join} from 'path';
 import {singleMatrix, multipleMatrices} from '../mock-data.js';
 import {makeReport} from '../../lib/generate.js';
+import {getConfig} from '../../lib/config.js';
 import {shouldBeReport} from '../assertions.js';
 import {asyncReadFile, asyncWriteFile} from '../../lib/files.js';
 
 describe('generate', async function() {
+  let config;
+  before(async function() {
+    config = await getConfig();
+  });
   it('should render a matrix', async function() {
-    const report = await makeReport({suite: singleMatrix});
+    const report = await makeReport({suite: singleMatrix, config});
     await asyncWriteFile(join(process.cwd(), '../single-report.html'), report);
     const expectedReport = await asyncReadFile(
       join(process.cwd(), '../single-report.html'));
     shouldBeReport(report, expectedReport.toString());
   });
   it('should render multiple matrices', async function() {
-    const report = await makeReport({suite: multipleMatrices});
+    const report = await makeReport({suite: multipleMatrices, config});
     await asyncWriteFile(join(process.cwd(), '../multiple-report.html'), report);
     const expectedReport = await asyncReadFile(
       join(process.cwd(), '../multiple-report.html'));
