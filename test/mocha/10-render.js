adc98eaf48b59ca1d0af54a89f9b3e662d962faf
Andrew Jones <ajones@digitalbazaar.com>
2022-05-10 15:20:15 -0400

    Use app-root-path in tests & remove html from tests.

--- test/mocha/10-render.js
+++ test/mocha/10-render.js
@@ -7,24 +7,27 @@ import {makeReport} from '../../lib/generate.js';
 import {getConfig} from '../../lib/config.js';
 import {shouldBeReport} from '../assertions.js';
 import {asyncReadFile, asyncWriteFile} from '../../lib/files.js';
+import appRoot from 'app-root-path';
 
 describe('generate', async function() {
   let config;
+  let rootPath;
   before(async function() {
     config = await getConfig();
+    rootPath = appRoot.toString();
   });
   it('should render a matrix', async function() {
     const report = await makeReport({suite: singleMatrix, config});
-    await asyncWriteFile(join(process.cwd(), '../single-report.html'), report);
+    await asyncWriteFile(join(rootPath, 'test/single-report.html'), report);
     const expectedReport = await asyncReadFile(
-      join(process.cwd(), '../single-report.html'));
+      join(rootPath, 'test/single-report.html'));
     shouldBeReport(report, expectedReport.toString());
   });
   it('should render multiple matrices', async function() {
     const report = await makeReport({suite: multipleMatrices, config});
-    await asyncWriteFile(join(process.cwd(), '../multiple-report.html'), report);
+    await asyncWriteFile(join(rootPath, 'test/multiple-report.html'), report);
     const expectedReport = await asyncReadFile(
-      join(process.cwd(), '../multiple-report.html'));
+      join(rootPath, 'test/multiple-report.html'));
     shouldBeReport(report, expectedReport.toString());
   });
 });
