102507f3df76a3904ca4be8cf963dc3cde6da9f2
Andrew Jones <ajones@digitalbazaar.com>
2023-07-20 14:14:41 +0000

    Rename --report to --suite & pass in defaults for config.

--- bin/makeReport.js
+++ bin/makeReport.js
@@ -4,27 +4,35 @@
  * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
  */
 import {argv} from 'node:process';
+import {getConfig} from '../lib/config.js';
 import {getJson} from '../lib/files.js';
 import {makeReport} from '../lib/generate.js';
 
 async function main() {
   console.log('w3c interop reporter make report cli');
-  console.log('options: --report=./path/to/report');
+  console.log('options: --suite=./path/to/suite');
   console.log('options: --config=./path/to/config');
   console.log('options: --output=./output.html');
-  // first 2 args are always node and file location
-  const commands = argv.slice(2);
   // capture the command and path
   const commandParser = /^-{2}(?<command>\D+)=(?<path>.+)$/i;
-  // print process commands
-  for(const command of commands) {
-    console.log({command});
-    console.log(commandParser.test(command));
-    console.log(commandParser.exec(command));
-  }
+  // first 2 args are always node and file location
+  const commands = await argv.slice(2).reduce(async (acc, current) => {
+    const parsed = commandParser.exec(current);
+    if(!parsed) {
+      throw new Error(`invalid command ${current}`);
+    }
+    const {groups} = parsed;
+    const json = await getJson(groups.path);
+    acc[groups.command] = json;
+    return acc;
+  }, {});
+  console.log({commands});
   const html = await makeReport({
-    // no stats
-    stats: []
+    // no stats by default
+    stats: [],
+    // use default config
+    config: getConfig(),
+    ...commands
   });
   // print out the html
   console.log(html);
