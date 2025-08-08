67c88ef1dbff52559b657d66df0b099d752260e2
Benjamin Young <byoung@digitalbazaar.com>
2025-08-07 20:32:52 -0400

    Upgrade all deps except eslint.
    Changed package.json loading to use createRequire.

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
-import pkg from './package.json';
+import {createRequire} from 'node:module';
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
 
 export default {
   input: './lib/index.js',
